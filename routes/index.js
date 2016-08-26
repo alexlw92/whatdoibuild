bebed00c5ed4ecfbf9147b0445ae1b26ea83152b
Alex Wang <alexlw92@yahoo.com>
2016-08-26 03:54:57 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var request = require("request")
+
+
+/* GET home page. */
+
+
+router.get('/', function(req, res, next) {
+	if (req.query.champion == null) {
+		res.render('lookup', { title: 'Express' });
+	}
+
+	else {
+		champion = req.query.champion
+
+		var startingUrl = "http://api.champion.gg/champion/"+champion+"/items/starters/mostPopular?api_key=6073c69232cbf21a6b565f6dbadf23e6"
+		var finishedUrl = "http://api.champion.gg/champion/"+champion+"/items/finished/mostPopular?api_key=6073c69232cbf21a6b565f6dbadf23e6"
+		var itemsURL = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=gold&locale=en_US&api_key=3a5fa3f0-c714-4a0a-9dab-955dcdc04bca"
+
+		// get list of all items
+		request({url: itemsURL, json: true
+		}, function (error, response, allItems) {
+			if (!error && response.statusCode === 200) {
+				
+				// console.log(allItems.data)
+				// get the finished items
+				request({url: finishedUrl, json: true
+				}, function (error, response, finishedItems) {
+					if (!error && response.statusCode === 200) {
+						//console.log(finishedItems) // Print the json response
+
+						// get the starting items
+						request({url: startingUrl, json: true
+						}, function (error, response, startingItems) {
+							if (!error && response.statusCode === 200) {
+								startingFinal = allItems.data[startingItems[0].items[0]].name
+								finishedFinal = []
+								finishedItems[0].items.forEach(function(value, index){
+									finishedFinal.push(allItems.data[value].name)
+								})
+
+ 								console.log(finishedFinal) // Print the json response
+								res.render('lookup', { 
+									title: 'Express', 
+									startingItem: startingFinal, 
+									finalItems: finishedFinal});
+							}
+							else
+								return
+						})
+						// end starting items
+
+					}
+					else
+						return
+				})
+				//end finished items
+
+			}
+			else 
+				return
+		})
+		// end list of all items
+
+	}
+
+
+	
+});
+
+module.exports = router;
